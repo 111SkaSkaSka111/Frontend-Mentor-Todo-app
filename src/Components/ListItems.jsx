c729bae39f10b899249e1c0316303dafabc05c08
sumberalam <skareggae1998@gmail.com>
2024-06-22 08:31:11 +0700

    add title

--- src/Components/ListItems.jsx
+++ src/Components/ListItems.jsx
@@ -6,7 +6,9 @@ const ListItems = ({ item, handleDelete, handleCheck }) => {
                     <input type="checkbox" className="check" onChange={() => handleCheck(item.id)} checked={item.checked} />
                     <div className="checkbox-custom"></div>
                 </div>
-                <p className="text">{item.text}</p>
+                <p className="text" title={item.text}>
+                    {item.text}
+                </p>
             </label>
             <div onClick={() => handleDelete(item.id)} className="">
                 <img src="/Frontend-Mentor-Todo-app/images/icon-cross.svg" alt="" />
